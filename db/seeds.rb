d96ed1041136c6d542b7a177f21e6b1d7faf019b
Outdam <mstdam@gmail.com>
2017-12-14 16:37:11 +0700

    Integrated nested attribute Technology in Portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ puts "3 topics created."
 
 10.times do |blog|
   Blog.create!(
-		title: "My Blog Post #{blog}",
-		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-		topic_id: Topic.last.id
+    title: "My Blog Post #{blog}",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    topic_id: Topic.last.id
   )
 end
 puts '10 blogs created.'
