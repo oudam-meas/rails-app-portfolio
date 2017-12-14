d1df31f2323e69e5b9e4474f4e46f435144a3157
Outdam <mstdam@gmail.com>
2017-12-14 15:17:53 +0700

    Implemetned setting defauld value , when empty

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,14 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio, -> { where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_defaults # after initialize is call when "new" action is called in the controller
+
+  def set_defaults
+    self.main_image ||='http://via.placeholder.com/600x400'
+    self.thumb_image ||='http://via.placeholder.com/350x200'
+  end
+  # ||=  means if main_image == nil
+  #   main_image = "http://via.placeholder.com/600x400"
+  
 end
