e15cb20a9e48770596a8a516cea5e6452835d7d3
Outdam <mstdam@gmail.com>
2017-12-14 08:46:17 +0700

    Implemented friendly routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :blogs
-
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
