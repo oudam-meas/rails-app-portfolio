34047c0705116819c6e3286bae8a2ec2ad9f4c29
Outdam <mstdam@gmail.com>
2017-12-15 14:03:05 +0700

    Refactored application controller to included concren for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   # Devise User Parameter Whitelist Include
   include DeviseWhitelist
-
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  include SetSource
 
 end
