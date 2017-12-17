868b2f15f808be6397924e29d7b1fac4b895353b
Outdam <mstdam@gmail.com>
2017-12-17 19:09:13 +0700

    Implemented content tag helper for view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
       (link_to  "Signup", new_user_registration_path)
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thank for visiting me from #{session[:source]} and you are on #{layout_name} layout"
+      content_tag :p, greeting, class: "source-greeting"
+    end
+  end
+
 end
