8f9b12704490b469aff364cf6bfff5c9ed78464d
Outdam <mstdam@gmail.com>
2017-12-20 12:09:16 +0700

    Implemented View Help for nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to  "Login", new_user_session_path, class: style) + " " +
+      (link_to  "Login", new_user_session_path, class: style) + " ".html_safe +
       (link_to  "Signup", new_user_registration_path, class: style)
     else
       link_to  "Logout", destroy_user_session_path, method: :delete, class: style
@@ -17,5 +17,42 @@ module ApplicationHelper
 
   def copyright_generator
       PortfolioModuleViewTool::Renderer.copyright 'Meas Outdam', 'All right reserved'
+  end
+  def nav_items
+    # Hash
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      {
+        url: about_me_path,
+        title: 'About'
+      },
+    ]
+  end
+  #link_to  "Login", new_user_session_path, class: style
+  def nav_helper style, tag
+    nav_links = '';
+      nav_items.each do |nav|
+         nav_links << "<#{tag}><a href='#{nav[:url]}' class='#{style} #{active? nav[:url]}'>#{nav[:title]}</a></#{tag}>".html_safe
+      end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
