c0de7f0b358ba0ee7a8915aeb5bc793485725cd9
Outdam <mstdam@gmail.com>
2017-12-20 08:37:16 +0700

    Implemeted Portfolio design parts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to  "Login", new_user_session_path, class: style) +
+      (link_to  "Login", new_user_session_path, class: style) + " " +
       (link_to  "Signup", new_user_registration_path, class: style)
     else
       link_to  "Logout", destroy_user_session_path, method: :delete, class: style
