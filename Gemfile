e181334f5836098e533343608831e6d3db24b865
Outdam <mstdam@gmail.com>
2017-12-21 12:10:58 +0700

    Implemented Image update with CerirerWave

--- Gemfile
+++ Gemfile
@@ -60,3 +60,6 @@ gem 'petergate', '~> 1.8', '>= 1.8.1'
 gem "font-awesome-rails"
 gem 'kaminari', '~> 1.1', '>= 1.1.1'
 gem 'jquery-ui-rails', '~> 6.0', '>= 6.0.1'
+gem 'dotenv', '~> 2.2', '>= 2.2.1'
+gem 'carrierwave', '~> 1.2', '>= 1.2.1'
+gem 'mini_magick', '~> 4.8'
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,10 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
   include Placeholder
+  validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
-  validates_presence_of :title, :subtitle, :body
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
   # Custome Scope
   def self.angular
