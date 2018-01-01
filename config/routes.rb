2a403bf7c15a1541293a63a5405dca74ab6ae000
Outdam <mstdam@gmail.com>
2018-01-01 21:25:47 +0700

    Created a comment broadbase, configure the websocket route, and updated the comment model with  a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :comments
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
+
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'angular', to: 'portfolios#angular'
   get 'about-me', to: 'pages#about'
@@ -15,6 +18,9 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
