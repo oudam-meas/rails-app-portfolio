339f409cbffcf5f52a574298ed29dfccf0856197
Outdam <mstdam@gmail.com>
2018-01-04 16:08:52 +0700

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://devcamp-portfolio.herokuapp.com', 'http://railsmz-portfolio.herokuapp.com']
+    config.action_cable.url = "wss://railsmz-portfolio.herokuapp.com/cable"
+
 end
